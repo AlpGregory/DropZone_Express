3f3777713e164916f618259447904fb4c3b3cf53
Gregory Alpizar <gregory.alce2001@gmail.com>
2023-08-24 17:30:06 -0600

    Set postgresql as default DB

--- src/dropzone_express/settings.py
+++ src/dropzone_express/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'dropzone_express.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dropzone_express',
+        'USER': 'alpizarg',
+        'PASSWORD': 'pizzas',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
